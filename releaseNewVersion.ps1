e2ed72704fd54a734ea94472e9c58e9420fc2e18
Franz Zeilinger <franz.zeilinger@siemens.com>
2025-07-17 20:00:12 +0200

    Refactor release script to improve step organization and enhance version update process

--- releaseNewVersion.ps1
+++ releaseNewVersion.ps1
@@ -1,6 +1,7 @@
 Write-Host "STARTING VERSION RELEASE"
 Write-Host "========================"
 Write-Host ""
+
 # Step 1: Merge commits to 'release' branch
 $currentBranch = git rev-parse --abbrev-ref HEAD
 if ($currentBranch -ne "main") {
@@ -9,11 +10,9 @@ if ($currentBranch -ne "main") {
     Write-Host "Aborting the script."
     Exit
 }
-
 # Get the last commit message of 'main':
 $lastCommitMessage = git log -1 --pretty=format:%s
-
-# Step 2: Check if the 'release' branch is checked out and merge 'main' into it
+# Check if the 'release' branch is checked out and merge 'main' into it
 git checkout release
 $currentBranch = git rev-parse --abbrev-ref HEAD
 if ($currentBranch -ne "release") {
@@ -24,12 +23,11 @@ if ($currentBranch -ne "release") {
 git pull
 git merge main
 
-# Step 3: Increase version number and commit
+# Step 2: Increase version number and get all information for the new version
 $readmePath = "README.md"
 $currentVersionPattern = "(?<=## Current Version\r?\n\r?\nv)(.+)"
 $readmeContent = Get-Content $readmePath -Raw
 $version = [regex]::Match($readmeContent, $currentVersionPattern).Groups[1].Value
-
 # Check if the new version number has a trailing flag
 $versionParts = $version -split "-"
 if ($versionParts.Length -gt 1) {
@@ -39,26 +37,21 @@ if ($versionParts.Length -gt 1) {
     $versionWithoutFlag = $version
     $flag = 0
 }
-
 # Get the tag message from the tag with the versionWithoutFlag
 $lastTagMessage = git tag -l --format='%(contents)' "v$versionWithoutFlag" | Out-String
 $lastTagMessage = $lastTagMessage.Trim()
-
 # Prompt the user to enter the new version number or hit Enter to keep the same version
 $newVersionWithoutFlag = Read-Host "Enter the new version number (current version: $versionWithoutFlag) [hit Enter to keep the same version]"
 $isVersionDifferent = $false
-
 # Validate the new version number or check if the user wants to keep the same version
 if (-not [string]::IsNullOrWhiteSpace($newVersionWithoutFlag)) {
     if (-not ($newVersionWithoutFlag -match '^\d+\.\d+\.\d+$')) {
         Write-Host "Invalid version number. Please enter a valid version number in the format X.Y.Z."
         Exit
     }
-
     # Check if the new version is different from the current version
     $currentVersionParts = $versionWithoutFlag -split "\."
     $newVersionParts = $newVersionWithoutFlag -split "\."
-
     for ($i = 0; $i -lt 3; $i++) {
         if ([int]$newVersionParts[$i] -gt [int]$currentVersionParts[$i]) {
             $isVersionDifferent = $true
@@ -70,7 +63,6 @@ if (-not [string]::IsNullOrWhiteSpace($newVersionWithoutFlag)) {
 } else {
     $newVersionWithoutFlag = $versionWithoutFlag
 }
-
 if (-not $isVersionDifferent) {
     # Prompt for confirmation to keep the same version
     Write-Host ""
@@ -84,7 +76,6 @@ if (-not $isVersionDifferent) {
         Exit
     }
 }
-
 if (-not $isVersionDifferent) {
     # If the version is not different, use the last tag message as the default commit message
     $defaulCommitMessage = $lastTagMessage
@@ -95,7 +86,6 @@ else {
     $defaulCommitMessage = $lastCommitMessage
     $messageSource = 'commit'
 }
-
 # Prompt the user to enter a custom tag message
 $customTagMessage = Read-Host "Enter a custom tag message [hit Enter to use the last $messageSource message '$defaulCommitMessage']"
 if ([string]::IsNullOrWhiteSpace($customTagMessage)) {
@@ -104,7 +94,6 @@ if ([string]::IsNullOrWhiteSpace($customTagMessage)) {
 } else {
     $tagMessage = $customTagMessage
 }
-
 # Add a flag to the version number, if same version is re-released
 if (-not $isVersionDifferent) {
     $flag = $flag + 1
@@ -113,19 +102,27 @@ if (-not $isVersionDifferent) {
     $newVersion = $newVersionWithoutFlag
 }
 
-# Step 4: Update the version number in the README file
+# Step 3: Update the version number in the README.md and package.json file
 $updatedReadmeContent = $readmeContent -replace $currentVersionPattern, $newVersion
 $streamWriter = [System.IO.StreamWriter]::new($readmePath, $false)
 $streamWriter.Write($updatedReadmeContent.Trim())
 $streamWriter.Close()
 git add $readmePath
+# update the version in package.json
+$packageJsonPath = "package.json"
+$packageJsonContent = Get-Content $packageJsonPath -Raw
+$packageJsonContent = $packageJsonContent -replace '"version":\s*".+?"', "`"version`": `"$newVersionWithoutFlag`""
+$streamWriter = [System.IO.StreamWriter]::new($packageJsonPath, $false)
+$streamWriter.Write($packageJsonContent.Trim())     
+$streamWriter.Close()
+git add $packageJsonPath
 if (-not $isVersionDifferent) {
     git commit -m "FIX: re-release version (v$newVersion)"
 } else {
     git commit -m "CHORE: release new version (v$newVersion)"
 }
 
-# Step 5: Tag commit and move 'latest' tag
+# Step 4: Tag commit and move 'latest' tag
 git tag -d latest
 git push origin :refs/tags/latest
 if (-not $isVersionDifferent) {
@@ -136,12 +133,12 @@ if (-not $isVersionDifferent) {
 git tag -a v$newVersionWithoutFlag -m "$tagMessage"
 git tag -a latest -m "Latest tagged version"
 
-# Step 6: Push commit and tag
+# Step 5: Push commit and tag
 git push
 git push origin v$newVersionWithoutFlag
 git push origin latest
 
-# Step 7: Switch back to the 'main' branch
+# Step 6: Switch back to the 'main' branch
 git checkout main
 
 Write-Host ""
